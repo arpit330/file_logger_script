26d577e25d03a71d2800f64d12361462c3f7de66
arpit-330 <arpitsahu644@gmail.com>
2023-12-18 22:28:53 +0530

    commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use('/public', express.static('public'));
 app.use(express.json());
 
 //convert the express app to socket server
-const server = app.listen(3000, () => {
+const server = app.listen(3000,'0.0.0.0', () => {
     console.log('listening on *:3000');
 });
 
@@ -26,7 +26,23 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+const logFilePath = __dirname + '/public/file.txt';
+function getLastLines(filePath, numLines) {
+    const fileContent = fs.readFileSync(filePath, 'utf8').split('\n');
+    const start = Math.max(fileContent.length - numLines, 0);
+    return fileContent.slice(start).join('\n');
+}
+
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+    fs.watch(logFilePath, (eventType) => {
+        if (eventType === 'change') {
+            // Send the new lines to the client upon file change
+            const Data = getLastLines(logFilePath,10);
+            socket.emit('filechanged', Data);
+        }
+    });
+
 });
 
--- public/client.js
+++ public/client.js
@@ -1,6 +1,19 @@
 const socket = io('http://127.0.0.1:3000');
 console.log("connected");
 
+// const div = document.getElementById('result');
+
+
+function display_logs(data) {
+    document.getElementById('result-container').innerHTML = '';
+    const lines = data.split('\n');
+    lines.forEach(line => {
+        document.getElementById('result-container').innerHTML += '<p>' + line + '</br><p>';
+    });
+};
+
 socket.on('filechanged', data => {
     console.log(data);
+    display_logs(data);
 })
+
